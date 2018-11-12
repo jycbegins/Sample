18f3c2f9ef7d24292bc72d8ebb841edcace418e3
jiangyucheng <jycbegins@163.com>
2018-11-12 08:24:06 +0000

    finish

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,5 +1,5 @@
 <li>
-  <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
+  <img src="" alt="{{ $user->name }}" class="gravatar"/>
   <a href="{{ route('users.show', $user->id )}}" class="username">{{ $user->name }}</a>
 
   @can('destroy', $user)
