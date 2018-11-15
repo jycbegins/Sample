382d448fc49aa134348b8dc4332beb7184b01a8d
jiangyucheng <jycbegins@163.com>
2018-11-15 07:44:37 +0000

    Add account activation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,9 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
 
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => $faker->safeEmail,
         'is_admin' => false,
-        //'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        //'remember_token' => str_random(10),
+        'activated' => true,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
         'created_at' => $date_time,
